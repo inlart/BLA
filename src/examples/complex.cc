ffc3dad513e95e5086fb6dfae0b4d5afedae1591
Martin Hartl <hello@inlart.com>
2018-04-30 17:07:09 +0200

    complex example

---
+++ src/examples/complex.cc
@@ -0,0 +1,23 @@
+#include <allscale/api/user/data/matrix.h>
+#include <complex>
+#include <cstdlib>
+
+using namespace allscale::api::user::data;
+
+using point_type = GridPoint<2>;
+using data_type = std::complex<double>;
+
+int main() {
+	Matrix<data_type> m1({10, 15});
+	Matrix<data_type> m2({15, 7});
+
+	m1.fill({2, 3});
+
+	m2.fill({3, 7});
+
+	Matrix<data_type> m3 = m1 * m2;
+
+	assert_eq(m3.size(), (point_type{m1.rows(), m2.columns()}));
+
+	return EXIT_SUCCESS;
+}
