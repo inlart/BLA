a6e36936cc5181ffbb859cade9c977e5120168b5
Martin Hartl <hello@inlart.com>
2018-06-18 10:25:45 +0200

    move simplify and transpose tests to own file

---
+++ src/test/user/data/simplify.cc
@@ -0,0 +1,268 @@
+#include <Vc/Vc>
+#include <allscale/api/user/data/matrix.h>
+#include <complex>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <type_traits>
+
+namespace allscale {
+namespace api {
+namespace user {
+namespace data {
+namespace impl {
+
+template <typename E1, typename E2, typename T = double>
+std::enable_if_t<!std::is_same<scalar_type_t<E1>, std::complex<double>>::value, bool> isAlmostEqual(const MatrixExpression<E1>& a,
+                                                                                                    const MatrixExpression<E2>& b, T epsilon = 0.001) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(coordinate_type i = 0; i < a.rows(); ++i) {
+        for(coordinate_type j = 0; j < a.columns(); ++j) {
+            scalar_type_t<E1> diff = (a[{i, j}] - b[{i, j}]);
+            if(diff * diff > epsilon) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+template <typename E1, typename E2, typename T = double>
+std::enable_if_t<std::is_same<scalar_type_t<E1>, std::complex<double>>::value, bool> isAlmostEqual(const MatrixExpression<E1>& a, const MatrixExpression<E2>& b,
+                                                                                                   T epsilon = 0.001) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(coordinate_type i = 0; i < a.rows(); ++i) {
+        for(coordinate_type j = 0; j < a.columns(); ++j) {
+            scalar_type_t<E1> diff = (a[{i, j}] - b[{i, j}]);
+            if(diff.real() * diff.real() > epsilon || diff.imag() * diff.imag() > epsilon) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+TEST(Simplify, Transpose) {
+    Matrix<int> m1({55, 58});
+    Matrix<int> m2({55, 58});
+    Matrix<int> m3({55, 58});
+    Matrix<int> m4({55, 58});
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 10);
+
+    auto g = [&](const auto&) { return dis(gen); };
+
+    m1.fill_seq(g);
+    m2.fill_seq(g);
+    m3.zero();
+    m4.zero();
+
+    m3 = m1 + m2;
+
+    m4 = simplify(m1 + m2);
+
+    ASSERT_EQ(m3, m4);
+
+    m3 = m1.transpose().transpose();
+    m4 = simplify(m1.transpose().transpose());
+
+    ASSERT_EQ(m3, m4);
+}
+
+TEST(Simplify, RecursiveTranspose) {
+    Matrix<int> m1({55, 58});
+    Matrix<int> m2({55, 58});
+    Matrix<int> m3({55, 58});
+    Matrix<int> m4({55, 58});
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 10);
+
+    auto g = [&](const auto&) { return dis(gen); };
+
+    m1.fill_seq(g);
+    m2.fill_seq(g);
+    m3.zero();
+    m4.zero();
+
+    m3 = m1 + m2.transpose().transpose();
+
+    m4 = simplify(m1 + m2.transpose().transpose());
+
+    ASSERT_EQ(m3, m4);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m1 + m2)>, std::decay_t<decltype(simplify(m1 + m2.transpose().transpose()))>>::value));
+}
+
+TEST(Simplify, MatrixScalarScalarMultiplication) {
+    Matrix<int> m1({55, 55});
+    Matrix<int> m2({55, 55});
+    Matrix<int> m3({55, 55});
+
+    m1.identity();
+    m2.zero();
+    m3.zero();
+
+    m2 = m1 * 5 * 6;
+
+    m3 = simplify(m1 * 5 * 6);
+
+    ASSERT_EQ(m2, m3);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m1 * 30)>, std::decay_t<decltype(simplify(m1 * 5 * 6))>>::value));
+}
+
+TEST(Simplify, ScalarScalarMatrixMultiplication) {
+    Matrix<int> m1({55, 55});
+    Matrix<int> m2({55, 55});
+    Matrix<int> m3({55, 55});
+
+    m1.identity();
+    m2.zero();
+    m3.zero();
+
+    m2 = 5 * (6 * m1);
+
+
+    m3 = simplify(5 * (6 * m1));
+
+    ASSERT_EQ(m2, m3);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(30 * m1)>, std::decay_t<decltype(simplify(5 * (6 * m1)))>>::value));
+}
+
+TEST(Simplify, ScalarMatrixScalarMultiplication1) {
+    Matrix<int> m1({55, 55});
+    Matrix<int> m2({55, 55});
+    Matrix<int> m3({55, 55});
+
+    m1.identity();
+    m2.zero();
+    m3.zero();
+
+    m2 = 6 * (m1 * 5);
+
+    m3 = simplify(6 * (m1 * 5));
+
+    ASSERT_EQ(m2, m3);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m1)>, std::decay_t<decltype(simplify(6 * (m1 * 5)).getExpression())>>::value));
+}
+
+TEST(Simplify, ScalarMatrixScalarMultiplication2) {
+    Matrix<int> m1({55, 55});
+    Matrix<int> m2({55, 55});
+    Matrix<int> m3({55, 55});
+
+    m1.identity();
+    m2.zero();
+    m3.zero();
+
+    m2 = (m1 * 5) * 6;
+
+    m3 = simplify((m1 * 5) * 6);
+
+    ASSERT_EQ(m2, m3);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m1)>, std::decay_t<decltype(simplify((m1 * 5) * 6).getExpression())>>::value));
+}
+
+TEST(Simplify, Negation) {
+    Matrix<int> m1({55, 58});
+    Matrix<int> m2({55, 58});
+    Matrix<int> m3({55, 58});
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 9);
+
+    auto g = [&](const auto&) { return dis(gen); };
+
+    m1.fill_seq(g);
+    m2.zero();
+    m3.zero();
+
+    m2 = -(-m1);
+
+    m3 = simplify(-(-m1));
+
+
+    ASSERT_EQ(m2, m3);
+
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m1)>, std::decay_t<decltype(simplify(-(-m1)))>>::value));
+}
+
+TEST(Simplify, IdentityMatrix) {
+    Matrix<int> m1({55, 58});
+    Matrix<int> m2({58, 55});
+    IdentityMatrix<int> m3(point_type{55, 55});
+    IdentityMatrix<int> m4(point_type{55, 55});
+
+    Matrix<int> r1({55, 58});
+    Matrix<int> r2({58, 55});
+    Matrix<int> r3({55, 55});
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 9);
+
+    auto g = [&](const auto&) { return dis(gen); };
+
+    m1.fill_seq(g);
+    m2.fill_seq(g);
+
+    r1 = simplify(m3 * m1);
+
+    ASSERT_EQ(m1, r1);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m1)>, std::decay_t<decltype(simplify(m3 * m1))>>::value));
+
+    r2 = simplify(m2 * m3);
+
+    ASSERT_EQ(m2, r2);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m2)>, std::decay_t<decltype(simplify(m3 * m2))>>::value));
+
+    r3 = simplify(m3 * m4);
+
+    ASSERT_EQ(m3, r3);
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(m3)>, std::decay_t<decltype(simplify(m3 * m4))>>::value));
+}
+
+TEST(Simplify, SubMatrixMultiplication) {
+    Matrix<int> m1({55, 58});
+    Matrix<int> m2({58, 55});
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 9);
+
+    auto g = [&](const auto&) { return dis(gen); };
+
+    m1.fill_seq(g);
+    m2.fill_seq(g);
+
+    BlockRange range({4, 7}, {13, 27});
+
+    Matrix<int> result = m1 * m2;
+
+    ASSERT_EQ(result.sub(range), (m1 * m2).sub(range));
+
+    ASSERT_TRUE((std::is_same<std::decay_t<decltype(static_cast<const Matrix<int>&>(m1).sub(std::declval<BlockRange>())
+                                                    * static_cast<const Matrix<int>&>(m2).sub(std::declval<BlockRange>()))>,
+                              std::decay_t<decltype(simplify((m1 * m2).sub(std::declval<BlockRange>())))>>::value));
+}
+
+} // end namespace impl
+} // end namespace data
+} // end namespace user
+} // end namespace api
+} // end namespace allscale
---
+++ src/test/user/data/transpose.cc
@@ -0,0 +1,100 @@
+#include <Vc/Vc>
+#include <allscale/api/user/data/matrix.h>
+#include <complex>
+#include <gtest/gtest.h>
+#include <iostream>
+#include <type_traits>
+
+namespace allscale {
+namespace api {
+namespace user {
+namespace data {
+namespace impl {
+
+template <typename E1, typename E2, typename T = double>
+std::enable_if_t<!std::is_same<scalar_type_t<E1>, std::complex<double>>::value, bool> isAlmostEqual(const MatrixExpression<E1>& a,
+                                                                                                    const MatrixExpression<E2>& b, T epsilon = 0.001) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(coordinate_type i = 0; i < a.rows(); ++i) {
+        for(coordinate_type j = 0; j < a.columns(); ++j) {
+            scalar_type_t<E1> diff = (a[{i, j}] - b[{i, j}]);
+            if(diff * diff > epsilon) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+template <typename E1, typename E2, typename T = double>
+std::enable_if_t<std::is_same<scalar_type_t<E1>, std::complex<double>>::value, bool> isAlmostEqual(const MatrixExpression<E1>& a, const MatrixExpression<E2>& b,
+                                                                                                   T epsilon = 0.001) {
+    if(a.size() != b.size()) {
+        return false;
+    }
+    for(coordinate_type i = 0; i < a.rows(); ++i) {
+        for(coordinate_type j = 0; j < a.columns(); ++j) {
+            scalar_type_t<E1> diff = (a[{i, j}] - b[{i, j}]);
+            if(diff.real() * diff.real() > epsilon || diff.imag() * diff.imag() > epsilon) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+TEST(Operation, Transpose) {
+    Matrix<double> m1({47, 39});
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(-1, 1);
+
+    auto g = [&](const auto&) { return dis(gen); };
+    m1.fill_seq(g);
+    Matrix<double> m2 = m1.transpose();
+
+    ASSERT_EQ(m1.rows(), m2.columns());
+    ASSERT_EQ(m2.rows(), m1.columns());
+
+    algorithm::pfor(m1.size(), [&](const point_type& p) { ASSERT_EQ(m1[p], (m2[{p.y, p.x}])); });
+}
+
+TEST(Operation, TransposeFloat) {
+    Matrix<float> m1({47, 39});
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dis(-1, 1);
+
+    auto g = [&](const auto&) { return dis(gen); };
+    m1.fill_seq(g);
+    Matrix<float> m2 = m1.transpose();
+
+    ASSERT_EQ(m1.rows(), m2.columns());
+    ASSERT_EQ(m2.rows(), m1.columns());
+
+    algorithm::pfor(m1.size(), [&](const point_type& p) { ASSERT_EQ(m1[p], (m2[{p.y, p.x}])); });
+}
+
+TEST(Operation, TransposeInt) {
+    Matrix<int> m1({47, 39});
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 9);
+
+    auto g = [&](const auto&) { return dis(gen); };
+    m1.fill_seq(g);
+    Matrix<int> m2 = m1.transpose();
+
+    ASSERT_EQ(m1.rows(), m2.columns());
+    ASSERT_EQ(m2.rows(), m1.columns());
+
+    algorithm::pfor(m1.size(), [&](const point_type& p) { ASSERT_EQ(m1[p], (m2[{p.y, p.x}])); });
+}
+
+} // end namespace impl
+} // end namespace data
+} // end namespace user
+} // end namespace api
+} // end namespace allscale
