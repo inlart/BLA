bff00b93444183a3a501572c43f31db07d3d6589
Martin Hartl <hello@inlart.com>
2018-06-18 16:43:44 +0200

    add lu decomposition benchmark

---
+++ src/benchmark/lud/lud_eigen.cc
@@ -0,0 +1,30 @@
+#include <Eigen/Dense>
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <random>
+
+#ifndef BENCHMARK_MIN_SIZE
+#define BENCHMARK_MIN_SIZE 128
+#endif
+
+#ifndef BENCHMARK_MAX_SIZE
+#define BENCHMARK_MAX_SIZE 2048
+#endif
+
+using Eigen::MatrixXd;
+
+static void benchmark_lud_eigen(benchmark::State& state) {
+    const int n = state.range(0);
+
+    MatrixXd a = MatrixXd::Random(n, n);
+    //    MatrixXd lower(n, n);
+    //    MatrixXd upper(n, n);
+
+    for(auto _ : state) {
+        benchmark::DoNotOptimize(a.lu());
+    }
+}
+
+BENCHMARK(benchmark_lud_eigen)->RangeMultiplier(2)->Range(BENCHMARK_MIN_SIZE, BENCHMARK_MAX_SIZE)->UseRealTime();
+
+BENCHMARK_MAIN();
