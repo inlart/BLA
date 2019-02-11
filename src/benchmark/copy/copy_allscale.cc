042bf6643abc6617ea6f624ab529ba3d6093b97e
Martin Hartl <hello@inlart.com>
2019-02-11 16:40:41 +0100

    add copy benchmark

---
+++ src/benchmark/copy/copy_allscale.cc
@@ -0,0 +1,38 @@
+#include <allscale/api/user/data/matrix.h>
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <random>
+
+#ifndef BENCHMARK_MIN_SIZE
+#define BENCHMARK_MIN_SIZE 128
+#endif
+
+#ifndef BENCHMARK_MAX_SIZE
+#define BENCHMARK_MAX_SIZE 2048
+#endif
+
+using Matrix = allscale::api::user::data::Matrix<double>;
+
+static void benchmark_add_allscale(benchmark::State& state) {
+    const int n = state.range(0);
+
+    Matrix a({n, n});
+    Matrix b({n, n});
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dis(-1, 1);
+
+    auto g = [&]() { return dis(gen); };
+
+    a.fill_seq(g);
+    b.fill_seq(g);
+
+    for(auto _ : state) {
+        benchmark::DoNotOptimize(a = b);
+    }
+}
+
+BENCHMARK(benchmark_add_allscale)->RangeMultiplier(2)->Range(BENCHMARK_MIN_SIZE, BENCHMARK_MAX_SIZE)->UseRealTime();
+
+BENCHMARK_MAIN();
---
+++ src/benchmark/copy/copy_eigen.cc
@@ -0,0 +1,29 @@
+#include <Eigen/Core>
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <random>
+
+#ifndef BENCHMARK_MIN_SIZE
+#define BENCHMARK_MIN_SIZE 128
+#endif
+
+#ifndef BENCHMARK_MAX_SIZE
+#define BENCHMARK_MAX_SIZE 2048
+#endif
+
+using Eigen::MatrixXd;
+
+static void benchmark_add_eigen(benchmark::State& state) {
+    const int n = state.range(0);
+
+    MatrixXd a = MatrixXd::Random(n, n);
+    MatrixXd b = MatrixXd::Random(n, n);
+
+    for(auto _ : state) {
+        benchmark::DoNotOptimize(a = b);
+    }
+}
+
+BENCHMARK(benchmark_add_eigen)->RangeMultiplier(2)->Range(BENCHMARK_MIN_SIZE, BENCHMARK_MAX_SIZE)->UseRealTime();
+
+BENCHMARK_MAIN();
