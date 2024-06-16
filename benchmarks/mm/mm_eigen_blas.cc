af8c42ac005efc7110e5fc951735783736e8a52c
Martin Hartl <hello@inlart.com>
2024-06-16 13:57:37 +0200

    benchmarks: Add Eigen mm benchmark using BLAS

---
+++ benchmarks/mm/mm_eigen_blas.cc
@@ -0,0 +1,30 @@
+#define EIGEN_USE_BLAS
+#include <Eigen/Core>
+#include <benchmark/benchmark.h>
+#include <iostream>
+#include <random>
+
+#ifndef BENCHMARK_MIN_SIZE
+#define BENCHMARK_MIN_SIZE 128
+#endif
+
+#ifndef BENCHMARK_MAX_SIZE
+#define BENCHMARK_MAX_SIZE 2048
+#endif
+
+using Eigen::MatrixXd;
+
+static void benchmark_mm_eigen(benchmark::State& state) {
+    const int n = state.range(0);
+
+    MatrixXd a = MatrixXd::Random(n, n);
+    MatrixXd b = MatrixXd::Random(n, n);
+
+    for(auto _ : state) {
+        benchmark::DoNotOptimize((a * b).eval());
+    }
+}
+
+BENCHMARK(benchmark_mm_eigen)->RangeMultiplier(2)->Range(BENCHMARK_MIN_SIZE, BENCHMARK_MAX_SIZE)->UseRealTime();
+
+BENCHMARK_MAIN();
