3f80bf6a8b93c4805840138515f3d99ab3ea5d2e
Martin Hartl <hello@inlart.com>
2024-08-14 16:39:11 +0200

    benchmarks: Add blaze transpose

---
+++ benchmarks/transpose/transpose_blaze.cc
@@ -0,0 +1,33 @@
+
+#include <benchmark/benchmark.h>
+#include <blaze/Blaze.h>
+
+#ifndef BENCHMARK_MIN_SIZE
+#define BENCHMARK_MIN_SIZE 128
+#endif
+
+#ifndef BENCHMARK_MAX_SIZE
+#define BENCHMARK_MAX_SIZE 2048
+#endif
+
+using namespace blaze;
+
+static void benchmark_transpose_blaze(benchmark::State& state) {
+    const int n = state.range(0);
+
+    DynamicMatrix<double, rowMajor> a(n, n), b(n, n);
+
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            a(i, j) = rand<double>();
+        }
+    }
+
+    for(auto _ : state) {
+        benchmark::DoNotOptimize(b = trans(a));
+    }
+}
+
+BENCHMARK(benchmark_transpose_blaze)->RangeMultiplier(2)->Range(BENCHMARK_MIN_SIZE, BENCHMARK_MAX_SIZE)->UseRealTime();
+
+BENCHMARK_MAIN();
