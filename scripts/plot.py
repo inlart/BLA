46663acb4a80e5b7d6f6116fbd640754f1c29390
Martin Hartl <hello@inlart.com>
2024-08-16 22:28:30 +0200

    scripts: Add performance plots

--- scripts/plot.py
+++ scripts/plot.py
@@ -3,13 +3,14 @@
 import json
 import argparse
 import copy
+from enum import Enum
 
 colors = ["black", "blue", "brown", "cyan", "darkgray", "gray", "green", "lightgray", "lime", "magenta", "olive", "orange", "pink", "purple", "red", "teal", "violet", "yellow"]
 
 def graphToTex(graph):
     global colors
     print("\\begin{tikzpicture}[scale=0.6]")
-    print("\\begin{{axis}}[xlabel=Threads, ylabel={}, xmin=1, ymin=0, legend pos=outer north east]".format(graph.ylabel))
+    print("\\begin{{axis}}[xlabel={}, ylabel={}, xmode={}, xmin={}, ymin=0, legend pos=outer north east]".format(graph.xlabel, graph.ylabel, graph.xmode, graph.xmin))
     color = 0
     legend = []
     for key in graph.y:
@@ -17,68 +18,75 @@ def graphToTex(graph):
         legend.append(compiler)
         print("\\addplot[color={},mark=x] coordinates {{".format(colors[color]))
         color += 1
-        threads = 1
         for value in graph.y[key]:
-            print("({}, {})".format(threads, value))
-            threads += 1
+            print("({}, {})".format(value[0], value[1]))
         print("};")
     print("\\legend{{{}}}".format(",".join(legend)))
     print("\\end{axis}")
     print("\\end{tikzpicture}")
 
+class GraphType(Enum):
+    THREADS = 1
+    PERFORMANCE = 2
+
 class Graph:
-    def __init__(self, name, size, x):
+    def __init__(self, graphType, name):
         self.ylabel = "Runtime (ns)"
         self.name = name
-        self.size = size
-        self.x = x
+        self.graphType = graphType
+        self.xmin = 1
+        if graphType == GraphType.THREADS:
+            self.xlabel = "Threads"
+        else:
+            self.xlabel = "Matrix Size"
         self.y = {}
+        self.xmode = "normal"
 
     def insert(self, libName, x, y):
         if not libName in self.y:
             self.y[libName] = []
         values = self.y[libName]
-        assert(x == 0 or x == self.x[len(values)])
-        values.append(y)
+        # assert(x == 0 or x == self.x[len(values)])
+        values.append((int(x), int(y)))
 
     def apply(self, func):
         for key in self.y:
             values = self.y[key]
             for i in range(len(values)):
-                values[i] = func(values[i], self.x[i])
+                values[i] = func(values[i])
 
     def __str__(self):
-        return self.name + "/" + self.size
+        return self.name
 
 class Result:
     def __init__(self, data):
         self.graphs = {}
-        # Calculate max threads
-        self.max = 1
-        for benchmarkName in data:
-            for resultData in data[benchmarkName]:
-                for result in resultData["results"]:
-                    if result["num_threads"] > self.max:
-                        self.max = result["num_threads"]
         self.values = {}
         for benchmarkName in data:
             benchmarkData = data[benchmarkName]
             for resultData in benchmarkData:
                 libName = resultData["name"]
                 for result in resultData["results"]:
-                    numThreads = result["num_threads"]
+                    numThreads = result["num_threads"] if "num_threads" in result else None
                     resultGBenchmark = result["benchmark"]
                     for gBenchmark in resultGBenchmark["benchmarks"]:
                         benchmarkSize = gBenchmark["name"].split("/")[1]
-                        graph = self.getGraph(benchmarkName, benchmarkSize)
-                        graph.insert(libName, numThreads, gBenchmark["real_time"])
+                        graphType = GraphType.THREADS if numThreads is not None else GraphType.PERFORMANCE
+                        if graphType == GraphType.THREADS:
+                            xvalue = numThreads
+                            name = "{}/{}".format(benchmarkName, benchmarkSize)
+                        else:
+                            xvalue = benchmarkSize
+                            name = benchmarkName
+                        graph = self.getGraph(graphType, name)
+                        graph.insert(libName, xvalue, gBenchmark["real_time"])
                         assert(gBenchmark["time_unit"] == "ns")
 
-    def getGraph(self, name, size):
-        if (name, size) in self.graphs:
-            return self.graphs[(name, size)]
-        self.graphs[(name, size)] = Graph(name, size, list(range(1, self.max + 1)))
-        return self.graphs[(name, size)]
+    def getGraph(self, graphType, name):
+        if (graphType, name) in self.graphs:
+            return self.graphs[(graphType, name)]
+        self.graphs[(graphType, name)] = Graph(graphType, name)
+        return self.graphs[(graphType, name)]
 
     def summary(self):
         print("\\documentclass{article}")
@@ -88,26 +96,32 @@ class Result:
         for graphKey in self.graphs:
             graph = self.graphs[graphKey]
 
-            fastest = None
-            for key in graph.y:
-                v = graph.y[key][0]
-                if not fastest or v < fastest:
-                    fastest = v
+            if graph.graphType == GraphType.THREADS:
+                fastest = None
+                for key in graph.y:
+                    v = graph.y[key][0][1]
+                    if not fastest or v < fastest:
+                        fastest = v
 
-            # speed up
-            speedup_graph = copy.deepcopy(graph)
-            speedup_graph.apply(lambda value, _: fastest / value)
-            speedup_graph.ylabel = "Speed up"
+                # speed up
+                speedup_graph = copy.deepcopy(graph)
+                speedup_graph.apply(lambda value: (value[0], fastest / value[1]))
+                speedup_graph.ylabel = "Speed up"
 
-            # efficiency
-            efficiency_graph = copy.deepcopy(graph)
-            efficiency_graph.apply(lambda value, threads: (fastest / value) / threads)
-            efficiency_graph.ylabel = "Efficiency"
+                # efficiency
+                efficiency_graph = copy.deepcopy(graph)
+                efficiency_graph.apply(lambda value: (value[0], (fastest / value[1]) / value[0]))
+                efficiency_graph.ylabel = "Efficiency"
 
-            print("\\section{{{}}}".format(graphKey))
-            graphToTex(graph)
-            graphToTex(speedup_graph)
-            graphToTex(efficiency_graph)
+                print("\\section{{{}}}".format(graph.name))
+                graphToTex(graph)
+                graphToTex(speedup_graph)
+                graphToTex(efficiency_graph)
+            else:
+                graph.apply(lambda value: (value[0], (2 * value[0] * value[0] * value[0] - value[0] * value[0]) / value[1]))
+                graph.ylabel = "GFLOPS"
+                graph.xmode = "log"
+                graphToTex(graph)
         print("\\end{document}")
 
 def calculate(json_data):
